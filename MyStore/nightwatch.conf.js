c5c213f788e84921f7f14e6e0ac5e311edc22dcd
Nada Elkady <84160576+Nada-Elkady@users.noreply.github.com>
2023-07-25 03:37:47 +0300

    Update nightwatch.conf.js

--- MyStore/nightwatch.conf.js
+++ MyStore/nightwatch.conf.js
@@ -2,9 +2,14 @@ module.exports = {
     "src_folders": ["mystoretests"],
  
     "webdriver":{
-        "start_process" : true,
-        "server_path" : require('chromedriver').path,
-        "port": 9515
+        start_process: true,
+     server_path: '/home/circleci/project/MyStore/node_modules/chromedriver/lib/chromedriver/chromedriver',
+     port: 9515,
+     host: 'localhost',
+     ssl: false,
+     default_path_prefix: '',
+     proxy: undefined,
+     cli_args: {}
     },
 
     "test_settings" :{
